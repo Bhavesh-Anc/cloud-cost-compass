77ec293a15d267fef0100875fe74490abb5a099c
Bhavesh Anchalia <anchaliabhavesh1@gmail.com>
2025-08-31 14:15:16 +0530

    Delete backend/api/init.py

--- backend/api/init.py
+++
@@ -1,5 +0,0 @@
-from .aws_pricing import get_aws_prices
-from .azure_pricing import get_azure_prices
-from .gcp_pricing import get_gcp_prices
-
-__all__ = ['get_aws_prices', 'get_azure_prices', 'get_gcp_prices']
